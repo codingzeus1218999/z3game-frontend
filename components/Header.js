16ab3585aa15ed5ae8b17c4ec2663ab46b3991a3
Marcus Haickel <vinicius.el@gmail.com>
2022-06-29 17:19:31 -0300

    layout changes on header

--- components/Header.js
+++ components/Header.js
@@ -18,10 +18,100 @@ const Header = () => {
   const { totalPrice, cartCount } = useShoppingCart();
 
   return (
-    
-    <header className="bg-white py-3 border-b">
 
-<div class="container max-w-screen-xl mx-auto px-4">
+    //<!-- This example requires Tailwind CSS v2.0+ --> 
+    <nav class="sticky top-0 z-50 bg-[#333333] shadow-lg">
+      <div class="max-w-screen-2xl  mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="relative flex items-center justify-between h-16">
+          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/*<!-- Mobile menu button-->*/}
+            <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+              <span class="sr-only">Open main menu</span>
+              {/*<!--
+                Icon when menu is closed.
+    
+                Heroicon name: outline/menu
+    
+                Menu open: "hidden", Menu closed: "block"
+              -->*/}
+              <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+              {/*<!--
+                Icon when menu is open.
+    
+                Heroicon name: outline/x
+    
+                Menu open: "block", Menu closed: "hidden"
+              -->*/}
+              <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+          <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div class="flex-shrink-0 flex items-center">
+              <Logo />
+            </div>
+            <div class="hidden sm:block sm:ml-6">
+              <div class="flex space-x-4">
+                {/*<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->*/}
+                <a href="#" class="text-gray-200 hover:text-white px-1 py-2 text-sm font-medium">Playstation</a>
+                <a href="#" class="text-gray-200 hover:text-white px-1 py-2 text-sm font-medium">Xbox</a>
+                <a href="#" class="text-gray-200 hover:text-white px-1 py-2 text-sm font-medium">Nintendo</a>
+                <a href="#" class="text-gray-200 hover:text-white px-1 py-2 text-sm font-medium">Jogos PC</a>
+                <a href="#" class="text-gray-200 hover:text-white px-1 py-2 text-sm font-medium">Jogos Mobile</a>
+                <a href="#" class="text-gray-200 hover:text-white px-1 py-2 text-sm font-medium">Google Play</a>
+                <a href="#" class="text-gray-200 hover:text-white px-1 py-2 text-sm font-medium">Serviços e Entretenimento</a>
+              </div>
+            </div>
+          </div>
+          <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          <button type="button" class="inline-block text-gray-200 hover:text-white">
+            <FontAwesomeIcon icon={faSearch}/>
+					</button>
+    
+
+            <div class="ml-3 relative">
+              <div>
+                <Link href="/cart">
+                  <a className="flex items-center space-x-1 text-gray-700 hover:text-gray-900">
+                    <div className="relative">
+                      <ShoppingCartIcon className="w-7 h-7 flex-shrink-0" />
+                    </div>
+                    <p className="text-lg">
+                      {formatCurrency(totalPrice)}{' '}
+                      <span className="text-sm text-gray-500">({cartCount})</span>
+                    </p>
+                  </a>
+                </Link>
+              </div>
+    
+
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    {/*<!-- Mobile menu, show/hide based on menu state. --> */}
+      <div class="sm:hidden" id="mobile-menu">
+        <div class="px-2 pt-2 pb-3 space-y-1">
+          {/*<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->*/}
+          <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
+    
+          <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
+    
+          <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+    
+          <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+        </div>
+      </div>
+    
+    
+    
+    {/*
+    <header className="sticky top-0 z-50 py-3 border-b bg-[#333333] shadow-lg">
+<div class="container max-w-screen-2xl mx-auto px-4">
 			<div class="flex flex-wrap items-center">
 				<div class="flex-shrink-0 mr-5">
         <Logo />
@@ -38,9 +128,8 @@ const Header = () => {
 
 				<div class="flex items-center space-x-2 ml-auto">
 					<a class="px-3 py-2 inline-block text-center text-gray-700 bg-white shadow-sm border border-gray-200 rounded-md hover:bg-gray-100 hover:border-gray-300" href="#"> 
-            
+          <FontAwesomeIcon icon={faUser} class="svg-inline--fa fa-user text-gray-400 w-5" />
 						<span class="hidden lg:inline ml-1">
-              <FontAwesomeIcon icon={faUser} class="svg-inline--fa fa-user text-gray-400 w-5" />
               Sign in</span>
 					</a>
 
@@ -75,29 +164,11 @@ const Header = () => {
 			</div>
 		</div> 
 
-
-
-
-
-
-
-      {/* <div className="container xl:max-w-screen-xl mx-auto p-6 flex justify-between">
-        <Logo />
-        <Link href="/cart">
-          <a className="flex items-center space-x-1 text-gray-700 hover:text-gray-900">
-            <div className="relative">
-              <ShoppingCartIcon className="w-7 h-7 flex-shrink-0" />
-            </div>
-            <p className="text-lg">
-              {formatCurrency(totalPrice)}{' '}
-              <span className="text-sm text-gray-500">({cartCount})</span>
-            </p>
-          </a>
-        </Link>
-      </div> */}
-
-
     </header>
+    */}
+
+</nav>
+
   );
 };
 
