e56f7c8d9542fc56f1b507bfb297a1c149f1f2b8
codingzeus1218999 <codingzeus1218999@gmail.com>
2024-02-09 09:14:19 +0200

    Made productionmenuitem for desktop

--- components/menuItem/index.js
+++ components/menuItem/index.js
@@ -1,13 +1,17 @@
 import Image from "next/image";
+import { motion } from "framer-motion";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
 import styles from "./style.module.scss";
 
 const MenuItem = ({ image = "", title = "" }) => {
   return (
-    <div className="text-white">
-      <Image src={image} alt={title} />
+    <div className={styles.container}>
+      <Image src={image} alt={title} width={15} />
       {title}
+      <motion.div>
+        <ChevronDownIcon className={styles.chevrondown} />
+      </motion.div>
     </div>
   );
 };
