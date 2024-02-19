b8516bb6393a21e90b178f0f3bc14f54763c2c2f
codingzeus1218999 <codingzeus1218999@gmail.com>
2024-02-19 05:55:29 +0200

    Improved the popular title

--- components/popularList/index.js
+++ components/popularList/index.js
@@ -1,12 +1,23 @@
+import Link from "next/link";
 import PopularCard from "../popularCard";
 import styles from "./style.module.scss";
 
 export default function PopularList({ list }) {
   return (
     <div className={styles.container}>
-      {list.map((l, _idx) => (
-        <PopularCard product={l} key={_idx} number={_idx + 1} />
-      ))}
+      <div className={styles.headerContainer}>
+        <p className={styles.title}>Most Popular</p>
+        <Link href="/">
+          <div className="al-button-primary rounded-none whitespace-nowrap">
+            view more
+          </div>
+        </Link>
+      </div>
+      <div className={styles.cardsContainer}>
+        {list.map((l, _idx) => (
+          <PopularCard product={l} key={_idx} number={_idx + 1} />
+        ))}
+      </div>
     </div>
   );
 }
